4d4d3599809d503881639064a207bfa6b0f2b15d
Luca <l.zenobi@t4project.com>
2018-02-02 17:46:53 +0100

    Initial commit from swagger generator

---
+++ go/default.go
@@ -0,0 +1,13 @@
+package goserver
+
+import (
+	"net/http"
+)
+
+type Default struct {
+}
+
+func PersonsGet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+}
